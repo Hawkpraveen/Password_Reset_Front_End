6cb59d33ff96330f523352ddfe886cb0050f4874
Praveen Kumar <hawkpraveen33@gmail.com>
2024-06-04 22:14:20 +0530

    updated reset page ui

--- src/Pages/ResetPage.jsx
+++ src/Pages/ResetPage.jsx
@@ -6,7 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 const ResetPage = () => {
   const [password, setPassword] = useState();
-  const [confirmPassword,setConfirmPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
   const navigate = useNavigate();
   const { id, token } = useParams();
 
@@ -15,7 +15,7 @@ const ResetPage = () => {
     try {
       const response = await axios.post(
         `http://localhost:5000/api/reset-password/${id}/${token}`,
-        { password ,confirmPassword}
+        { password, confirmPassword }
       );
       response.then(navigate("/login-user"), toast.success(data.message));
     } catch (error) {
@@ -24,42 +24,56 @@ const ResetPage = () => {
     }
   };
   return (
-    <div className="d-flex justify-content-center align-items-center bg-gradient">
-      <div className="bg-white p-3 rounded w-28">
-        <h4>Reset password</h4>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              <strong>New Password</strong>
-            </label>
-            <input
-              type="password"
-              name="password"
-              className="form-control rounded-0"
-              id="password"
-              placeholder="enter your password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              <strong>Confirm Password</strong>
-            </label>
-            <input
-              type="password"
-              name="confirm password"
-              className="form-control rounded-0"
-              id="password"
-              placeholder="enter your password"
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="btn btn-success w-100 rounded-0">
-            Update
+    
+      <div className="d-flex justify-content-center align-items-center container mt-5">
+        <div className="bg-white p-3 mt-5 card">
+        <legend className="fs-2 text-center ">
+          <span className=" text-info"> Demo App</span>
+        </legend>
+           <legend className="fs-2 text-center ">
+          <strong>Reset Password </strong>
+        </legend>
+          <form onSubmit={handleSubmit}>
+            <div  className="mb-3 text-center col-12 col-md-12 col-sm-12 d-flex justify-content-center pt-2 fs-4">
+            <div className="col-12 col-md-9 col-sm-12 ">
+              <label htmlFor="password" className="form-label">
+                <strong>New Password</strong>
+              </label>
+              <input
+                type="password"
+                name="password"
+                className="form-control mt-2 fs-5"
+                id="password"
+                placeholder="Enter your password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            </div>
+
+            <div className="mb-3 text-center col-12 col-md-12 col-sm-12 d-flex justify-content-center pt-2 fs-4">
+            <div className="col-12 col-md-9 col-sm-12">
+              <label htmlFor="password" className="form-label">
+                <strong>Confirm Password</strong>
+              </label>
+              <input
+                type="password"
+                name="confirm password"
+                className="form-control mt-2 fs-5"
+                id="password"
+                placeholder="Enter Confirm Password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+            </div>
+            <div className="col-12 col-md-12 col-sm-12 d-flex justify-content-center mt-4 mb-3">
+          <button type="submit" className="btn btn-primary col-md-4 col-sm-4 col-8">
+            Send
           </button>
-        </form>
+          </div>
+          </form>
+        </div>
       </div>
-    </div>
+    
   );
 };
 
